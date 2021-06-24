48f53a5c86639c2ad89caa19b186ecb4e102f1f2
Brandixitor <med.salah.bourouba@gmail.com>
2021-06-24 14:13:35 +0100

    Update

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -48,7 +48,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
 
             // calculate move direction to pass to character
             if (m_Cam != null)
@@ -64,7 +64,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+	        if (Input.GetKey(KeyCode.LeftAlt)) m_Move *= 0.5f;
+            if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 2f;
 #endif
 
             // pass all parameters to the character control script
